ed6fe0b04e2a2bef7f2cabff1c6017d416afea18
SergeyD <sevladem@gmail.com>
2019-03-19 12:38:51 +0200

    add output console

--- skills/facebook_postback.js
+++ skills/facebook_postback.js
@@ -4,7 +4,8 @@ var menu = require('../helpers/menu.js')
 module.exports = function(controller) {
 
   controller.on('facebook_postback', function(bot, message) {
-
+    console.log('facebook_postback');
+    console.log(message);
     if (menu[message.payload]) {
       bot.reply(message, menu[message.payload]);
     } else {
@@ -16,6 +17,8 @@ module.exports = function(controller) {
   controller.on('message_received', function(bot, message) {
 
       if (message.quick_reply) {
+        console.log('message_received/quick_reply');
+        console.log(message);
         if (menu[message.quick_reply.payload]){
           bot.reply(message, menu[message.quick_reply.payload]);
           //Возможно неправильно, но пока так.
